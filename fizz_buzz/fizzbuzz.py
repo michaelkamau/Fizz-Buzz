470839826b44a564ad2688ad16818a1fe98ebc82
Michael Kamau <tokamau@gmail.com>
2016-10-09 22:25:56 +0300

    Refactor the function name fizzbuzz to fizz_buzz

--- fizz_buzz/fizzbuzz.py
+++ fizz_buzz/fizzbuzz.py
@@ -1,13 +1,13 @@
-def fizzbuzz(num):
+def fizz_buzz(num):
     """
-    The function fizzbuzz receives a single argument and returns a value based on the following rules:
+    The function fizz_buzz receives a single argument and returns a value based on the following rules:
         - 'Fizz' if the argument is divisible by 3 only
         - 'Buzz' if the argument id divisible by 5 only
         - 'FizzBuzz' if the argument is divisible by both 3 and 5
         - otherwise, returns the arg passed
 
     :param: num: Numerical value
-    
+
     :return: value depending on the rules above
     """
     if num % 3 == 0 and num % 5 != 0:
--- test/fizzbuzztests.py
+++ test/fizzbuzztests.py
@@ -1,5 +1,5 @@
 import unittest
-from fizz_buzz.fizzbuzz import fizzbuzz
+from fizz_buzz.fizzbuzz import fizz_buzz
 
 
 class FizzBuzzOutcomes(unittest.TestCase):
@@ -7,25 +7,25 @@ class FizzBuzzOutcomes(unittest.TestCase):
         """
         Fizzbuzz should return Fizz if the arg is divisible by 3 only
         """
-        self.assertEqual('Fizz', fizzbuzz(9))
+        self.assertEqual('Fizz', fizz_buzz(9))
 
     def test_fizzbuzz_returns_correct_buzz(self):
         """
         Fizzbuzz should return Buzz if the arg is divisible by 5 only
         """
-        self.assertEqual('Buzz', fizzbuzz(100))
+        self.assertEqual('Buzz', fizz_buzz(100))
 
     def test_fizzbuzz_returns_correct_fizzbuzz(self):
         """
         Fizzbuzz should return FizzBuzz if the arg is divisible by both 3 and 5 only
         """
-        self.assertEqual('FizzBuzz', fizzbuzz(15))
+        self.assertEqual('FizzBuzz', fizz_buzz(15))
 
     def test_fizzbuzz_returns_original_arg(self):
         """
         Fizzbuzz should return the arg passed if it is not divisible by both 3 and 5
         """
-        self.assertEqual(34, fizzbuzz(34))
+        self.assertEqual(34, fizz_buzz(34))
 
 
 if __name__ == '__main__':
