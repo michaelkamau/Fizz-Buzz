b8283684e56637ae68edd03a36f7fedece60f6e5
Michael Kamau <tokamau@gmail.com>
2016-10-09 22:07:52 +0300

    Add test for Buzz

--- test/fizzbuzztests.py
+++ test/fizzbuzztests.py
@@ -1,12 +1,20 @@
 import unittest
 from fizz_buzz.fizzbuzz import fizzbuzz
 
+
 class FizzBuzzOutcomes(unittest.TestCase):
     def test_fizzbuzz_returns_correct_fizz(self):
         """
         Fizzbuzz should return Fizz if the arg is divisible by 3 only
         """
         self.assertEqual('Fizz', fizzbuzz(9))
+
+    def test_fizzbuzz_returns_correct_buzz(self):
+        """
+        Fizzbuzz should return Buzz if the arg is divisible by 5 only
+        """
+        self.assertEqual('Buzz', fizzbuzz(100))
+
 
 if __name__ == '__main__':
     unittest.main()
